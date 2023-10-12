2309240d08b7f1e973ef2a40499e5f7f06aa5a1b
ascarbek <ascarbek@gmail.com>
2023-10-12 14:19:22 +0600

    replace client response with object

--- src/Client/oneSocketClient.ts
+++ src/Client/oneSocketClient.ts
@@ -1,18 +1,34 @@
 import * as net from 'node:net';
 import { z } from 'zod';
+import { TResponse } from '../types.js';
 
 const incomingSchema = z.object({
   type: z.string(),
   payload: z.any(),
 });
 
-export const OneSocketClient: (host: string, port: number, params: any) => Promise<string> = (host, port, params) => {
-  return new Promise((resolve, reject) => {
+const outgoingSchema = z.discriminatedUnion('success', [
+  z.object({
+    success: z.literal(true),
+    payload: z.any(),
+  }),
+  z.object({
+    success: z.literal(false),
+    errorMsg: z.string(),
+  }),
+]);
+
+export const OneSocketClient: (host: string, port: number, params: any) => Promise<TResponse<any>> = (
+  host,
+  port,
+  params
+) => {
+  return new Promise((resolve) => {
     const incomingParseResult = incomingSchema.safeParse(params);
     if (!incomingParseResult.success) {
       const errors = incomingParseResult.error.errors;
       console.log(errors);
-      return reject(errors[errors.length - 1].message);
+      return resolve({ success: false, errorMsg: errors[errors.length - 1].message });
     }
 
     const client = net.createConnection({ host, port }, async () => {
@@ -27,12 +43,23 @@ export const OneSocketClient: (host: string, port: number, params: any) => Promi
       }
       client.end();
 
-      resolve(buffer);
+      try {
+        const response = JSON.parse(buffer);
+        const outgoingParseResult = outgoingSchema.safeParse(response);
+        if (!outgoingParseResult.success) {
+          const errors = outgoingParseResult.error.errors;
+          console.log(errors);
+          return resolve({ success: false, errorMsg: errors[errors.length - 1].message });
+        }
+        resolve(response);
+      } catch (e) {
+        resolve({ success: false, errorMsg: 'Invalid response payload' });
+      }
     });
 
     client.on('error', (err) => {
       console.log('oh oh...  ', err);
-      reject(err);
+      resolve({ success: false, errorMsg: 'Connection error' });
     });
   });
 };
